3e3d16b1948d6b220ebfb6894761f8c1dedf1462
Khushboo <guptakhushboo81537@gmail.com>
2022-02-15 18:05:16 +0530

    Project Structure refactored

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:covitendance/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const SplashScreen());
+    await tester.pumpWidget(const Covitendance());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
