a06dd491990501d04f414f59149ef188eb65ebf9
Khushboo <guptakhushboo81537@gmail.com>
2022-02-15 21:46:15 +0530

    Code refactored

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+//Sample Code for adding colors
+//Please follow the pattern of naming variables of color
+const Color darkText = Color.fromRGBO(0, 0, 0, 1.0);
+const Color lightText = Color.fromRGBO(255, 255, 255, 0.87);
+const Color background = Color.fromRGBO(225, 228, 229, 1.0);
+const Color lightGrey = Color.fromRGBO(0, 0, 0, 0.05);
+const Color favoritesGutterAccent = Color.fromRGBO(229, 55, 108, 1.0);
