582c14abdcc3928f22d66734ed686d228fcb8bdb
Khushboo <guptakhushboo81537@gmail.com>
2022-02-15 17:17:12 +0530

    Initialized project structure

---
+++ android/app/src/main/kotlin/com/example/covitendance/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.covitendance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SplashScreen());
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Text(
+      "Covitendance",
+    ));
+  }
+}
