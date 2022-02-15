40a01f1037595e6cad4de6fc43b03a572ab5ebca
Khushboo <guptakhushboo81537@gmail.com>
2022-02-16 00:01:20 +0530

    Modified Splash Screen

---
+++ lib/pages/signup_login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignUp_Login extends StatelessWidget {
+  const SignUp_Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text("SignUp and Login Buttons here"),
+        ),
+      ),
+    );
+  }
+}
