d812cde292b76e39ee3b601d98c39bcbe872dd37
Hiferli <joshi.ishaan.2001@gmail.com>
2022-02-16 13:40:03 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class Covitendance extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      body: SplashScreen(),
-    ));
+          body: SplashScreen(),
+        ));
   }
 }
