402c88d60ce95f8e635f4dd587336554deeaa787
Khushboo <guptakhushboo81537@gmail.com>
2022-02-16 13:48:01 +0530

    Code refactored

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class Covitendance extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
           body: SplashScreen(),
         ));
